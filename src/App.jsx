7e75a327e8c792afe8400c737a2bec1ee414baf0
Javier Carrasco <javierdeveloper08@gmail.com>
2023-03-30 08:30:25 -0400

    90%

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useContext } from "react";
+import { useState } from "react";
 import sun from "./assets/sun.svg";
 import moon from "./assets/moon.svg";
 import gitHub from "./assets/github.svg";
